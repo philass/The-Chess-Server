37ee28f466712761073f758986dad085719f30a6
Philip Lassen <philiplassen@gmail.com>
2019-04-13 23:35:09 +0200

    Tower moves work for white

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -1,13 +1,13 @@
 #include <iomanip>
 #include <iostream>
 #include <cmath>
-
+#include <vector>
 class ChessGame 
 {
   private:
     bool turn; // True means Whites turn
     bool gameOn;// True means game is not over
-    int board[8][8] =
+    std::vector<std::vector<int>> board =
       {{-1, -2, -3, -4, -5, -3, -2, -1},
        {-6, -6, -6, -6, -6, -6, -6, -6}, 
         {0,  0,  0,  0,  0,  0,  0,  0},
@@ -17,10 +17,26 @@ class ChessGame
         {6,  6,  6,  6,  6,  6,  6,  6},    
         {1,  2,  3,  4,  5,  3,  2,  1}}; 
     
-    bool canMoveDiag(rowFrom, colFrom, rowTo, colTo) {
-      return true;
+
+
+    bool canMoveDiag(int rowFrom, int colFrom, int rowTo, int colTo) {
+      if (rowTo < 0 || colTo < 0 || rowTo > 7 || colTo > 7) {
+        return false;
+      }
+
+      if (std:abs(rowTo - rowFrom) == std::abs(colTo - colFrom) && rowTo != rowFrom) {
+        int deltaCol = colTo > colFrom ? 1 : -1; 
+        int deltaRow = rowTo > rowFrom ? 1 : -1;
+        int currentRow = rowFrom;
+        int currentCol = colFrom;
+        while (currentRow != rowTo) {
+          currentRow += deltaRow;
+
+
+        }
+      }
     }
-    bool canMoveStraight(rowFrom, colFrom, rowTo, ColumnTo) {
+    bool canMoveStraight(int rowFrom, int colFrom, int rowTo, int colTo) {
       if (rowTo < 0 || colTo < 0 || rowTo > 7 || colTo > 7) {
         return false;
       }
@@ -31,10 +47,10 @@ class ChessGame
             return false;
           }
         }
-        if (board[rowTo][colTo] < 0) {
+        if (board[rowTo][colTo] > 0) {
           return false; 
         } else {
-          int* copy = copyBoard();
+          std::vector<std::vector<int>> copy = board;
           copy[rowTo][colTo] = copy[rowFrom][colFrom];
           copy[rowTo][colTo] = 0;
           return !inCheck(copy);
@@ -47,10 +63,10 @@ class ChessGame
             return false;
           }
         }
-        if (board[rowTo][colTo] < 0) {
+        if (board[rowTo][colTo] > 0) {
           return false; 
         } else {
-          int* copy = copyBoard();
+          std::vector<std::vector<int>> copy = board;
           copy[rowTo][colTo] = copy[rowFrom][colFrom];
           copy[rowTo][colTo] = 0;
           return !inCheck(copy);
@@ -58,14 +74,14 @@ class ChessGame
       }
 
     }
-    bool inCheck(int[8][8] boardState) 
+    bool inCheck(std::vector<std::vector<int>> boardState) 
     {
       return false;
     }
-    bool canMovePawn(rowFrom, colFrom, rowTo, colTo) {
+    bool canMovePawn(int rowFrom, int colFrom, int rowTo, int colTo) {
       return true;
     }
-    bool canMoveKing(rowFrom, colFrom, rowTo, colTo)
+    bool canMoveKing(int rowFrom, int colFrom, int rowTo, int colTo)
     {
       int colDif = std::abs(colFrom - colTo); 
       int rowDif = std::abs(rowFrom - rowTo); 
@@ -73,18 +89,18 @@ class ChessGame
       bool canMove = canMoveStraight(rowFrom, colFrom, rowTo, colTo) || 
         canMoveStraight(rowFrom, colFrom, rowTo, colTo);
     }
-    bool canMoveQueen(rowFrom, colFrom, rowTo, colTo)
+    bool canMoveQueen(int rowFrom, int colFrom, int rowTo, int colTo)
     {
       return canMoveStraight(rowFrom, colFrom, rowTo, colTo) || canMoveStraight(rowFrom, colFrom, rowTo, colTo);
     }
-    bool canMoveKnight(rowFrom, colFrom, rowTo, colTo) {
+    bool canMoveKnight(int rowFrom, int colFrom, int rowTo, int colTo) {
       return false; 
     }
-    bool canMoveBishop(rowFrom, colFrom, rowTo, colTo)
+    bool canMoveBishop(int rowFrom, int colFrom, int rowTo, int colTo)
     {
       return canMoveStraight(rowFrom, colFrom, rowTo, colTo);
     }
-    bool canMoveRook(rowFrom, colFrom, rowTo, colTo)
+    bool canMoveRook(int rowFrom, int colFrom, int rowTo, int colTo)
     {
       return canMoveStraight(rowFrom, colFrom, rowTo, colTo);
     }
@@ -104,42 +120,33 @@ class ChessGame
       }
     }
 
-    int* boardCopy() 
-    {
-      int* copy = new int[8][8];
-      for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-          copy[i][j] = board[i][j];
-        }
-      }
-      return copy
-    }
-
     void move(int rowFrom, int colFrom, int rowTo, int colTo)
     {
-      int *copy = boardCopy();
+      std::vector<std::vector<int>> copy = board;
       int piece = board[rowFrom][colFrom];
-      bool legal;
+      bool legal = false;
       if (piece == 0) {
         std::cout << "Illegal Move" << std::endl;   
         legal = false;
       } else if (piece == 1) {
-        legal = canMoveRook(rowFrom, colFrom, rowTo, ColumnTo);  
+        legal = canMoveRook(rowFrom, colFrom, rowTo, colTo);  
       } else if (piece == 2) {
-        legal = canMoveKnight(rowFrom, colFrom, rowTo, ColumnTo);
+        legal = canMoveKnight(rowFrom, colFrom, rowTo, colTo);
       } else if (piece == 3) {
-        legal = canMoveBishop(rowFrom, colFrom, rowTo, ColumnTo);
+        legal = canMoveBishop(rowFrom, colFrom, rowTo, colTo);
       } else if (piece == 4) {
-        legal = canMoveQueen(rowFrom, colFrom, rowTo, ColumnTo);
+        legal = canMoveQueen(rowFrom, colFrom, rowTo, colTo);
       } else if (piece == 5) {
-        legal = canMoveKing(rowFrom, colFrom, rowTo, ColumnTo);
+        legal = canMoveKing(rowFrom, colFrom, rowTo, colTo);
+      } else if (piece == 6) {
+        legal = canMovePawn(rowFrom, colFrom, rowTo, colTo);
       }
 
       // Need case for ampersant pond
       // Need cases for black pieces
       if (legal) {
         board[rowFrom][colFrom] = 0;
-        board[rowTo][rowFrom] = piece;
+        board[rowTo][colTo] = piece;
       }
       std::cout << "The piece selected is " << piece << std::endl;
       std::cout << "col : " << colFrom << " | row : " << rowFrom << std::endl;
@@ -150,6 +157,11 @@ int main()
 {
   ChessGame c;
   c.printBoard();
-  c.move(0, 6, 0, 0);
+  c.move(6, 7, 0, 0);
+  c.printBoard();
+  c.move(7, 7, 4, 7);
+  c.printBoard();
+  c.move(4, 7, 0, 7);
+  c.printBoard();
   return 0;
 }

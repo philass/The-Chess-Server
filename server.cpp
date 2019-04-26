a0f8bb24106409cb67202676034bd3dcc3a9e5a6
Philip Lassen <philiplassen@gmail.com>
2019-04-27 00:38:45 +0200

    Chess Server implemented with thread error

--- server.cpp
+++ server.cpp
@@ -103,11 +103,11 @@ int main()
     // socket create and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
+    //    printf("socket creation failed...\n"); 
         exit(0); 
     } 
     else
-        printf("Socket successfully created..\n"); 
+     //   printf("Socket successfully created..\n"); 
     bzero(&servaddr, sizeof(servaddr)); 
   
     // assign IP, PORT 
@@ -117,19 +117,19 @@ int main()
   
     // Binding newly created socket to given IP and verification 
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
+ //       printf("socket bind failed...\n"); 
         exit(0); 
     } 
     else
-        printf("Socket successfully binded..\n"); 
+  //      printf("Socket successfully binded..\n"); 
   
     // Now server is ready to listen and verification 
     if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
+   //     printf("Listen failed...\n"); 
         exit(0); 
     } 
     else
-        printf("Server listening..\n"); 
+    //    printf("Server listening..\n"); 
     len = sizeof(cli); 
     len1 = sizeof(cli1); 
     // Accept the data packet from client and verification 
@@ -139,18 +139,18 @@ int main()
     connfd1 = accept(sockfd, (SA*)&cli1, &socklen1); 
  
     if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
+     //   printf("server acccept failed...\n"); 
         exit(0); 
     } 
     else
-        printf("server acccept the client0...\n"); 
+        //printf("server acccept the client0...\n"); 
     
     if (connfd1 < 0) { 
-        printf("server acccept failed...\n"); 
+      //  printf("server acccept failed...\n"); 
         exit(0); 
     } 
     else
-        printf("server acccept the client1...\n"); 
+       // printf("server acccept the client1...\n"); 
   
     // Function for chatting between client and server 
     fun(connfd, connfd1, c); 
